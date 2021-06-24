a5a2d19ecb7f2613ce8e9ed951743ab5eeed1ca2
sajjad <sajjaddoodabi55@gmail.com>
2021-06-24 18:03:37 +0430

    adding background to the game

--- View.cpp
+++ View.cpp
@@ -4,10 +4,17 @@ View::View() : QGraphicsView()
 {
     //create scnen
     scene = new QGraphicsScene();
-    scene->setSceneRect(0, 0, 1400, 1000);
+    scene->setSceneRect(0, 0, 1920, 1080);
     setScene(scene);
+
+    //fullsceenig the game window
+    setWindowState(Qt::WindowFullScreen);
 
     //delete scroll bar
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    //adding background
+    setBackgroundBrush(QBrush(QImage(":/images/SpaceBackground2.jpg")));
+
 }

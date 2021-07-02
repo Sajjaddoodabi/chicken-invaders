871bc97f5af20065e3e96de256186ccade5d7f52
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-07-02 15:45:10 +0430

    add constructor in lives.cpp

--- Lives.cpp
+++ Lives.cpp
@@ -1,6 +1,16 @@
 #include "Lives.h"
+#include <QFont>
 
+// constructor
 Lives::Lives(QGraphicsItem *parent)
+  : QGraphicsTextItem (parent) , playerLives{3}
 {
+    // change playerScore to text
+    setPlainText(QString::number(playerLives));
 
+    // set text's color
+    setDefaultTextColor(Qt::white);
+
+    // set text's font
+    setFont(QFont("times", 20));
 }
--- Score.cpp
+++ Score.cpp
@@ -1,14 +1,18 @@
 #include "Score.h"
 #include <QFont>
 
+// constructor
 Score::Score(QGraphicsItem *parent)
   : QGraphicsTextItem (parent) , playerScore{0}
 {
+    // change playerScore to text
     setPlainText(QString::number(playerScore));
+
+    // set text's color
     setDefaultTextColor(Qt::white);
+
+    // set text's font
     setFont(QFont("times", 20));
-
-
 }
 
 // add to score function

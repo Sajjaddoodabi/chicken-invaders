3b8abbf9e94b65a5c8c18b578de41cbc8c88654e
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-07-02 17:10:46 +0430

    set picture for SpaceShip

--- SpaceShip.cpp
+++ SpaceShip.cpp
@@ -1,6 +1,8 @@
 #include "SpaceShip.h"
 
-SpaceShip::SpaceShip(QTimer *timer, QGraphicsItem *parent)
+SpaceShip::SpaceShip(QTimer *timer, QGraphicsItem *parent) : QObject(), QGraphicsPixmapItem(parent)
 {
-
+    // set picture
+    setPixmap(QPixmap(":/images/spaceship/image1.png"));
+    setPos(885, 890);
 }

cd508038ecb902484fdf4f7617283e64a606b8d0
sajjad <sajjaddoodabi55@gmail.com>
2021-07-02 13:35:55 +0430

    baby chicken : set pic

--- babyChicken.cpp
+++ babyChicken.cpp
@@ -1,6 +1,9 @@
 #include "babyChicken.h"
 
 babyChicken::babyChicken(int speedPerPix, QTimer *timer, QGraphicsItem *parent, int Healt)
+    : QObject() , QGraphicsPixmapItem(parent) , Health{Health} , speedPerPix{speedPerPix}
 {
+    //set picture for baby chicken
+    setPixmap(QPixmap(":/image/"));
 
 }

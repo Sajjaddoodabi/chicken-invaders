3c578a8f3c5f33c721bbcd2e170fd23700dcb3f2
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-07-02 14:51:56 +0430

    add level

--- Bullet.cpp
+++ Bullet.cpp
@@ -5,6 +5,7 @@
 Bullet::Bullet(QGraphicsItem *parent, QTimer *bulletTimer, QMediaPlayer *bulletSound, int x, int y)
     : QObject(), QGraphicsPixmapItem(parent), x{x}, y{y}
 {
-
+    // set level
+    level = 0;
 }
 

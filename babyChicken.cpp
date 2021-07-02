fc5197a68bc2b00bfb99168419503888f022c91b
sajjad <sajjaddoodabi55@gmail.com>
2021-07-02 13:38:33 +0430

    chickenBaby : add sound

--- babyChicken.cpp
+++ babyChicken.cpp
@@ -9,6 +9,17 @@ babyChicken::babyChicken(int speedPerPix, QTimer *timer, QGraphicsItem *parent, 
     //connect timer to move
     connect(timer , SIGNAL(timeout()) , this , SLOT(move()));
 
+    //add sound for the chicken
+    BabyChickenSound = new QMediaPlayer();
+    BabyChickenSound->setMedia(QUrl("qrc:/music/"));
+
+    if(BabyChickenSound->state() == QMediaPlayer::PlayingState){
+        BabyChickenSound->setPosition(0);
+    }
+    else if(BabyChickenSound->state() == QMediaPlayer::StoppedState){
+        BabyChickenSound->play();
+    }
+
 }
 
 void babyChicken::move()

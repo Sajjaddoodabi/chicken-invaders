07d7ef026a8d0e4baff9f618e9f9e6a3f58915a1
sajjad <sajjaddoodabi55@gmail.com>
2021-07-13 13:18:28 +0430

    Gift : add pic

--- Gift.cpp
+++ Gift.cpp
@@ -3,5 +3,6 @@
 Gift::Gift(QGraphicsScene *GiftScene, QTimer *timer, QGraphicsItem *parent) : QObject() ,
        QGraphicsPixmapItem (parent) , GiftScene(GiftScene)
 {
-
+    // set meat picture
+    setPixmap(QPixmap(":/images/")); //ToDO
 }

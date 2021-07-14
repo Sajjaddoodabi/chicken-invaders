66f9ed5248a15fd07e8a7eae46d303c6411d74f2
sajjad <sajjaddoodabi55@gmail.com>
2021-07-14 14:04:59 +0430

    MainMenuBotton : constructor , set pic

--- MainMenuBotton.cpp
+++ MainMenuBotton.cpp
@@ -1,6 +1,7 @@
 #include "MainMenuBotton.h"
 
-MainMenuBotton::MainMenuBotton()
+MainMenuBotton::MainMenuBotton(QGraphicsScene *scene) : QGraphicsPixmapItem(), buttonScene{scene}, click{0}
 {
-
+    // setting picture
+    setPixmap(QPixmap(":/images/menu/"));
 }
--- MainMenuBotton.h
+++ MainMenuBotton.h
@@ -1,13 +1,19 @@
 #ifndef MAINMENUBOTTON_H
 #define MAINMENUBOTTON_H
 #include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QMediaPlayer>
 
 class MainMenuBotton : public QGraphicsPixmapItem
 {
     friend class AfterLevel;
+private:
+    QGraphicsScene * buttonScene;
+    QMediaPlayer * buttonMedia;
+    int click;
 
 public:
-    MainMenuBotton();
+    MainMenuBotton(QGraphicsScene *scene);
 };
 
 #endif // MAINMENUBOTTON_H
